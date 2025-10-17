84f70e92cb694ab07701e3d10241ab388d1cb3d2
paul <paul@mavikcapital.com>
2025-10-16 21:18:13 -0400

    Refactor ApolloWrapper to memoize ApolloClient initialization and improve performance

--- frontend/src/components/ApolloWrapper.tsx
+++ frontend/src/components/ApolloWrapper.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useMemo } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { useAuth } from '../contexts/AuthContext';
@@ -11,38 +12,36 @@ const httpLink = createHttpLink({
 export function ApolloWrapper({ children }: { children: React.ReactNode }) {
   const { oidcToken } = useAuth();
 
-  const authLink = setContext((_, { headers }) => {
-    const authHeaders: Record<string, string> = {
-      ...headers,
-      'Content-Type': 'application/json',
-    };
+  const client = useMemo(() => {
+    const authLink = setContext((_, { headers }) => {
+      const authHeaders: Record<string, string> = {
+        ...headers,
+        'Content-Type': 'application/json',
+      };
 
-    if (oidcToken) {
-      authHeaders['x-amzn-oidc-data'] = oidcToken;
-      authHeaders['Authorization'] = oidcToken; // This is by design for the backend to read
-    }
+      if (oidcToken) {
+        authHeaders['x-amzn-oidc-data'] = oidcToken;
+        authHeaders['Authorization'] = oidcToken; // This is by design for the backend to read
+      }
 
-    return {
-      headers: authHeaders
-    };
-  });
+      return {
+        headers: authHeaders,
+      };
+    });
 
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-    defaultOptions: {
-      watchQuery: {
-        errorPolicy: 'all',
+    return new ApolloClient({
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache(),
+      defaultOptions: {
+        watchQuery: {
+          errorPolicy: 'all',
+        },
+        query: {
+          errorPolicy: 'all',
+        },
       },
-      query: {
-        errorPolicy: 'all',
-      },
-    },
-  });
+    });
+  }, [oidcToken]);
 
-  return (
-    <ApolloProvider client={client}>
-      {children}
-    </ApolloProvider>
-  );
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
